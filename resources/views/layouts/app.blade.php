fce72e20f3b563dae0165ad25e8a904762ae571c
Rafael Roman <rafaelroman1991@hotmail.com>
2018-12-11 20:07:17 -0500

    still stuck on errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,16 +34,16 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                           <li class="dropdown channel">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse<span class="caret"></span></a>
+                                {{-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse<span class="caret"></span></a> --}}
                                 <ul class="dropdown-menu">
                                     <li>
                                         <a href="/threads">All Threads</a>
                                     </li>
-                                    @if (auth()->check())
+                                   {{--  @if (auth()->check())
                                         <li>
                                             <a href="/threads?by={{ auth()->user()->name }}">My Threads</a>
                                         </li>
-                                    @endif
+                                    @endif --}}
                                 </ul>
                           </li>
 
@@ -54,9 +54,9 @@
                         <li class="dropdown channel">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Channels<span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                @foreach ($channels as $channel)
+{{--                                 @foreach ($channels as $channel)
                                     <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
-                                @endforeach
+                                @endforeach --}}    
                             </ul>
                         </li>
                     </ul>
